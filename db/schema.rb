cdde495299cf645d67b2c9c9d67e73e6d8cf7224
Emre Ozen <fossoyeuse@proton.me>
2023-05-18 21:19:42 -0400

    models done: user, instrument, user_instrument

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_000920) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_19_010904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "instruments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_instruments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "instrument_id", null: false
+    t.string "proficiency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_id"], name: "index_user_instruments_on_instrument_id"
+    t.index ["user_id"], name: "index_user_instruments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_000920) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_instruments", "instruments"
+  add_foreign_key "user_instruments", "users"
 end
