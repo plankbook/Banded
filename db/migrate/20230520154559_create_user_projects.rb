05f97316b541e5847caeef47159132abd655d638
arnaud lecorvaisier <arnaud.lecorvaisier@wanadoo.fr>
2023-05-20 11:50:03 -0400

    user_project model

---
+++ db/migrate/20230520154559_create_user_projects.rb
@@ -0,0 +1,11 @@
+class CreateUserProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_projects do |t|
+      t.boolean :admin
+      t.references :user, null: false, foreign_key: true
+      t.references :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
