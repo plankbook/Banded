af780a68b5dd1231463065b7d55b2d6e9dc4c854
arnaud lecorvaisier <arnaud.lecorvaisier@wanadoo.fr>
2023-05-21 16:08:20 -0400

    project routes are working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_012507) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_20_154559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_012507) do
     t.index ["user_id"], name: "index_user_instruments_on_user_id"
   end
 
+  create_table "user_projects", force: :cascade do |t|
+    t.boolean "admin"
+    t.bigint "user_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_user_projects_on_project_id"
+    t.index ["user_id"], name: "index_user_projects_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -75,4 +85,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_012507) do
   add_foreign_key "projects", "users"
   add_foreign_key "user_instruments", "instruments"
   add_foreign_key "user_instruments", "users"
+  add_foreign_key "user_projects", "projects"
+  add_foreign_key "user_projects", "users"
 end
---
+++ test/controllers/user_project_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UserProjectControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
