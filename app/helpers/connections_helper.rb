67f660726c1be7feb9fa38c0f58f23c308ef64d9
Emre Ozen <fossoyeuse@proton.me>
2023-05-31 23:32:30 -0400

    created modal for My Connections with edited styling

---
+++ app/helpers/connections_helper.rb
@@ -0,0 +1,12 @@
+module ConnectionsHelper
+    def my_connections
+        Connection.where('(requester_id = ? or receiver_id = ?) and status = ?', current_user, current_user, 'accepted')
+    end
+    def connections_requested
+        Connection.where(requester: current_user, status: "pending")
+    end
+
+    def connections_received
+        Connection.where(receiver: current_user, status: "pending")
+    end
+end  
