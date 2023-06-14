521f1b6e9d4a69f0d3ea479302c291ab0b66b5bd
robertli <robertg.li@yahoo.com>
2023-06-14 10:30:12 -0400

    solving authenticity issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include ConnectionsHelper
 
+  # protect_from_forgery unless: -> { request.format.json? }
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_notifications, if: :current_user
