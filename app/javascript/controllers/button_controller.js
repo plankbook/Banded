29739f2a35f8d08a9fca9c312a9b629b59e2ec69
robertli <robertg.li@yahoo.com>
2023-06-13 23:56:48 -0400

    solving disable button issue

--- app/javascript/controllers/button_controller.js
+++ app/javascript/controllers/button_controller.js
@@ -2,14 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="button"
 export default class extends Controller {
-  static targets = ["addProjectUserButton"]
+  static targets = ["button"]
 
   connect() {
     console.log("Hello from Button Controller!");
-    console.log(this.addProjectUserButtonTarget);
+    console.log(this.buttonTarget);
   }
 
+  // disable() {
+  //   // console.log(addProjectUserButtonTarget);
+  //   // this.addProjectUserButtonTarget.classList.add("submitted");
+  //   // this.addProjectUserButtonTarget.preventDefault();
+  // }
+
   disable() {
-    this.addProjectUserButtonTarget.classList.add("submitted");
+    // console.dir(this.buttonTarget);
+    // const form = new FormData();
+    fetch(this.buttonTarget.parentElement.action, {
+      method: "POST",
+      headers: { "Accept": "application/json" },
+      body: new FormData(this.buttonTarget.parentElement)
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+
+    this.buttonTarget.disabled = true;
   }
-}
+ }
